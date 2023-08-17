5cb859cccc6e4cf8839d89df252409e46d6452de
sysleec <sysleec@gmail.com>
2023-08-17 23:41:52 +0300

    added recursion to crawlPage for internal pages

--- crawl.js
+++ crawl.js
@@ -1,5 +1,53 @@
 const { JSDOM } = require('jsdom')
 
+
+async function crawlPage(baseURL, currentURL, pages) {
+    const currentUrlObj = new URL(currentURL)
+    const baseUrlObj = new URL(baseURL)
+    if (currentUrlObj.hostname !== baseUrlObj.hostname) {
+        return pages
+    }
+
+    const normalizedURL = normalizeURL(currentURL)
+
+    if (pages[normalizedURL] > 0) {
+        pages[normalizedURL]++
+        return pages
+    }
+
+    if (currentURL === baseURL) {
+        pages[normalizedURL] = 0
+    } else {
+        pages[normalizedURL] = 1
+    }
+
+    console.log(`crawling ${currentURL}`)
+    let htmlBody = ''
+    try {
+        const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.log(`Got HTTP error, status code: ${resp.status}`)
+            return pages
+        }
+        const contentType = resp.headers.get('content-type')
+        if (!contentType.includes('text/html')) {
+            console.log(`Got non-html response: ${contentType}`)
+            return pages
+        }
+        htmlBody = await resp.text()
+    } catch (err) {
+        console.log(err.message)
+    }
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+    for (const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURL, pages)
+    }
+
+    return pages
+}
+
+
 function normalizeURL(url) {
     const nURL = new URL(url)
     let fullUrl = `${nURL.host}${nURL.pathname}`
@@ -32,25 +80,6 @@ function getURLsFromHTML(htmlBody, baseURL) {
         }
     }
     return urls
-}
-
-async function crawlPage(currentURL) {
-    console.log(`Crawling ${currentURL}...`)
-    try {
-        const response = await fetch(currentURL)
-        if (response.status >= 400) {
-            console.log(`HTTP error. Status code - ${response.status}`)
-            return
-        }
-        const contType = response.headers.get('content-type')
-        if (!contType.includes('text/html')) {
-            console.log(`Non-HTML Response: ${contType}`)
-            return
-        }
-        console.log(await response.text())
-    } catch (err) {
-        console.log(err.message)
-    }
 }
 
 module.exports = {
