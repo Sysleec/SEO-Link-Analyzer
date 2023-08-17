c4276571e172fd97442d17918dc258f5b4f44ea6
sysleec <sysleec@gmail.com>
2023-08-17 22:24:48 +0300

    get urls from HTML func and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom')
+
 function normalizeURL(url) {
     const nURL = new URL(url)
     let fullUrl = `${nURL.host}${nURL.pathname}`
@@ -10,6 +12,31 @@ function normalizeURL(url) {
     return fullUrl
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const aElements = dom.window.document.querySelectorAll('a')
+    for (const aElement of aElements) {
+        if (aElement.href.slice(0, 1) === '/') {
+            try {
+                urls.push(new URL(aElement.href, baseURL).href)
+            } catch (err) {
+                console.log(`${err.message}: ${aElement.href}`)
+            }
+        } else {
+            try {
+                urls.push(new URL(aElement.href).href)
+            } catch (err) {
+                console.log(`${err.message}: ${aElement.href}`)
+            }
+        }
+    }
+    return urls
+}
+
+
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,6 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
+
 
 test('normalizeURL www', () => {
     expect(normalizeURL('https://www.nodejs.org/en')).toBe('nodejs.org/en')
@@ -12,4 +13,18 @@ test('normalizeURL http', () => {
 })
 test('normalizeURL additional characters', () => {
     expect(normalizeURL('https://nodejs.org/en/?&')).toBe('nodejs.org/en')
+})
+
+test('getURLsFromHTML absolute path', () => {
+    const baseUrl = 'https://nodejs.org/ru/about'
+    const body = '<html><body><a href="https://nodejs.org/ru/about/governance"><span>nodejs.org></span></a></body></html>'
+    const result = ['https://nodejs.org/ru/about/governance']
+    expect(getURLsFromHTML(body, baseUrl)).toEqual(result)
+})
+
+test('getURLsFromHTML both', () => {
+    const baseUrl = 'https://nodejs.org/ru/about'
+    const body = '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://nodejs.org/path/one"><span>nodejs.org></span></a></body></html>'
+    const result = ['https://nodejs.org/path/one', 'https://nodejs.org/path/one']
+    expect(getURLsFromHTML(body, baseUrl)).toEqual(result)
 })
